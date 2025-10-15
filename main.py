a38f99bf1a9d8946667058d03a9c2cec40d20fde
Raphael Fagundes <fagundesraphael7@gmail.com>
2025-10-15 17:04:05 -0400

    feat: basic HTTP proxy is working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import httpx
 
 app = FastAPI(title="gatekeepyr")
 
@@ -11,3 +12,10 @@ async def root():
 @app.get("/health")
 async def health():
     return {"status": "Ok"}
+
+
+@app.get("/proxy")
+async def proxy():
+    async with httpx.AsyncClient() as client:
+        r = await client.get("http://127.0.0.1:8001/")
+        return r.json()
