1c61f42d6d194a6bd75fd3c6c16b212762f2e3f6
Raphael Fagundes <fagundesraphael7@gmail.com>
2025-10-21 16:08:08 -0400

    feat: implement circuit breaker/auto-recovery

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import asyncio
+import logging
+import time
+from contextlib import asynccontextmanager
+from enum import Enum
+from itertools import cycle
+
+import httpx
 from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
-import httpx
-import logging
-from itertools import cycle
-from contextlib import asynccontextmanager
-
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -25,6 +27,32 @@ async def health_check_loop():
 
 app = FastAPI(lifespan=lifespan)
 
+
+class State(Enum):
+    CLOSED = "closed"
+    OPEN = "open"
+    HALF_OPEN = "half_open"
+
+
+circuit_breakers = {
+    "http://127.0.0.1:8001": {
+        "state": State.CLOSED,
+        "failures": 0,
+        "failure_time": None,
+    },
+    "http://127.0.0.1:8002": {
+        "state": State.CLOSED,
+        "failures": 0,
+        "failure_time": None,
+    },
+    "http://127.0.0.1:8003": {
+        "state": State.CLOSED,
+        "failures": 0,
+        "failure_time": None,
+    },
+}
+
+
 URLS = [
     "http://127.0.0.1:8001",
     "http://127.0.0.1:8002",
@@ -39,6 +67,9 @@ backend_metrics = {
 
 backend_urls = cycle(URLS)
 working_backends = URLS.copy()
+
+FAILURE_THRESHOLD = 3
+TIMEOUT_SECONDS = 30
 
 
 @app.get("/")
@@ -56,12 +87,40 @@ async def proxy(path: str):
     backend = next(backend_urls)
     backend_metrics[backend] += 1
     url = f"{backend}/{path}"
+    circuit = circuit_breakers[backend]
+
+    if circuit["state"] == State.OPEN:
+
+        if (
+            circuit["failure_time"]
+            and (time.time() - circuit["failure_time"]) >= TIMEOUT_SECONDS
+        ):
+            circuit["state"] = State.HALF_OPEN
+            logger.warning(f"Backend {backend} is now HALF_OPEN")
+        else:
+            raise HTTPException(503, f"Circuit breaker OPEN for {backend}")
+
     try:
         async with httpx.AsyncClient(timeout=5.0) as client:
             response = await client.get(url)
+
+            response.raise_for_status()
+            circuit["failures"] = 0
+            if circuit["state"] == State.HALF_OPEN:
+                circuit["state"] = State.CLOSED
+                logger.info(f"Circuit {backend} is now CLOSED")
             return response.json()
-    except httpx.RequestError as e:
-        logger.error(f"Backend service unavailable: {e}")
+
+    except (httpx.HTTPStatusError, httpx.RequestError) as e:
+        circuit["failures"] += 1
+        circuit["failure_time"] = time.time()
+
+        if circuit["failures"] >= FAILURE_THRESHOLD:
+            circuit["state"] = State.OPEN
+            logger.warning(
+                f"Circuit {backend} is now OPEN (failures: {circuit['failures']})"
+            )
+        logger.error(f"Backend {backend} service unavailable {e}:")
         raise HTTPException(503, "Service is not responding")
 
 
@@ -85,5 +144,8 @@ async def check_health():
                     new_working_backends.append(backend)
         except:
             logger.warning(f"Backend {backend} is down")
-    working_backends = new_working_backends
-    backend_urls = cycle(working_backends)
+    if new_working_backends:
+        working_backends = new_working_backends
+        backend_urls = cycle(working_backends)
+    else:
+        logger.error("All backends are down!")
